950afba114cf7ca23bddf1f55f6c7987f261b441
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-04-12 00:00:05 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const data=[ // jab multipe user ho na to use array ke under lena padta hai 
+
+{name:'Samrat Singh', email:'mynsamrat@gmail.com',concat:'897423232'}
+{name:'Aniket Kumar', email:'myns121amrat@gmail.com'}
+{name:'ANirudh Kumar', email:'myns121amrat@gmail.com'}
+{name:'Gopal', email:'mynsamrat@gm214ail.com'}
+{name:'Dipak Singh', email:'mynsam341rat@gmail.com'}
+{name:'Sankar Singh', email:'mynsa1245mrat@gmail.com'}
+{name:'Sambhu Singh', email:'myn356743samrat@gmail.com'}  
+    
+]
+module.exports=data;
